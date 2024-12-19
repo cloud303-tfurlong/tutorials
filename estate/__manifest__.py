3ff9506d3cd2f2730f78c30d2ad173f5f763761d
Tim Furlong <furlong.tim@gmail.com>
2024-12-18 17:03:56 -0700

    finished with chapter 3

---
+++ estate/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Real Estate",
+    "depends": [
+        # 'base_setup',
+        "base",
+    ],
+    # "data": [],
+    # "demo": [],
+    # "css": [],
+    "installable": True,
+    "application": True,
+    "auto_install": True,
+}
